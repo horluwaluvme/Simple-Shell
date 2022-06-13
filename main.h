cf2ae48149cfecc53d534deed0f78277648684ff
“horluwaluvme” <pymfemi@gmail.com>
2022-06-13 05:39:22 -0700

    main.h

---
+++ main.h
@@ -0,0 +1,58 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#define MAXCOM 1000
+#define BUFFER 1024
+#define MAXLIST 100
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <signal.h>
+#include <limits.h>
+#include <string.h>
+
+extern char **environ;
+char **_environ;
+char *callmemaybe;
+
+void execArg(char **command, char *name);
+int _strcmp(char *s1, char *s2);
+int storeinput(char *str);
+char *_strcpy(char *dest, char *src);
+void split_space(char *str, char **command);
+int _strlen(char *s);
+char *_strcat(char *dest, char *src);
+int cpathandexec(char **command, char *name);
+int cknowncommand(char **command);
+void _puts(char *str);
+int _putchar(char c);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+ssize_t _getline(char **lineptr, size_t *n, FILE *stream);
+void bring_line(char **lineptr, size_t *n, char *buf, size_t j);
+unsigned int is_delim(char c, char *delim);
+char *_strtok(char *str, char *delim);
+int _atoi(char *s);
+char *_strdup(char *str);
+char *remove_comment(char *str);
+char *copy_info(char *name, char *value);
+void set_env(char *name, char *value);
+int _csetenv(char **command);
+void set_data(char *name);
+char **_reallocdp(char **ptr, unsigned int old_size, unsigned int new_size);
+int _unsetenv(char **command);
+void rev_string(char *s);
+int cd_exec(char **command);
+void cd_to_home(void);
+void cd_previous(void);
+void cd_to(char **command);
+void cd_dot(char **command);
+char *_getenv(const char *name, char **_environ);
+int cmp_env_name(const char *nenv, const char *name);
+
+#endif
